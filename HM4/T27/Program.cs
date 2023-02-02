c47b2483b0ea3ee3f3cd26440f9bd960ecc86678
Alex Var <KvE1N@ya.ru>
2023-02-02 16:43:33 +0300

    new homework add

---
+++ HM4/T27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine("Введите число, чтобы посчитать сумму цифр в числе  ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+  int Sum(int numA)
+  {
+    int count = Convert.ToString(numA).Length;
+
+    int go = 0;
+
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      go = numA - numA % 10;
+      result = result + (numA - go);
+      numA = numA / 10;
+    }
+   return result;
+  }
+
+int sumNumbers = Sum(numA);
+
+Console.WriteLine("Сумма цифр в числе = " + sumNumbers);
---
+++ HM4/T29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите количество элементов массива ");
+int elementsCount = int.Parse(Console.ReadLine());
+int[] myArray = new int[elementsCount];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+  Console.Write($"Введите элемент {i}: ");
+  myArray[i] = int.Parse(Console.ReadLine());
+ }
+
+Console.Write("[");
+  for (int i = 0; i < myArray.Length; i++)
+ {
+   Console.Write(myArray[i] + ",");
+ }
+Console.Write("]");
+   Console.ReadLine();
